aeac9149f70ccd7a7bd9f9bef6254356dc55f798
Franco Vedia <francoaresis@gmail.com>
2023-11-20 16:37:02 -0300

    transaction added

--- src/main/java/com/app/whatsapp/ending/test/repository/IUserRepository.java
+++ src/main/java/com/app/whatsapp/ending/test/repository/IUserRepository.java
@@ -1,10 +1,12 @@
 package com.app.whatsapp.ending.test.repository;
 
 import com.app.whatsapp.ending.test.entity.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@Transactional
 public interface IUserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByPhone(String phone);
 }
